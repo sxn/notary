ef0f9dfd8188203647af35b50ba7edac5246cc05
Sorin Muntean <me@sorinmuntean.ro>
2017-03-02 16:31:38 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='notary',
+    version='0.0.2',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    include_package_data=True,
+    install_requires=['click', 'crayons'],
+    entry_points='''
+        [console_scripts]
+        notary=notary:cli
+    '''
+)
