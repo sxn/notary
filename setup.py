ef78d6ecc1ee96c7a56144a7ea8c164e2309ee4b
Sorin Muntean <me@sorinmuntean.ro>
2017-03-04 14:26:50 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='notary',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     include_package_data=True,
     install_requires=['click', 'crayons'],
